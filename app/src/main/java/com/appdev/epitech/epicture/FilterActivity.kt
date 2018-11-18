b8e2fb61718aceeed30b9c74831a7d46dec50901
julian ladjani <julian.ladjani@epitech.eu>
2018-11-18 02:25:16 +0100

    Tags

--- app/src/main/java/com/appdev/epitech/epicture/FilterActivity.kt
+++ app/src/main/java/com/appdev/epitech/epicture/FilterActivity.kt
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ImageButton
 import com.appdev.epitech.epicture.R.id.*
 import com.appdev.epitech.epicture.api.ImgurApi
+import com.google.android.material.chip.Chip
+import com.google.android.material.resources.MaterialResources
 import kotlinx.android.synthetic.main.activity_filter.*
 import kotlinx.android.synthetic.main.toolbar.view.*
 
-class FilterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class FilterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, View.OnClickListener {
+    override fun onClick(v: View?) {
+        if (v == addTagButton) {
+            val tag = editTag.text.toString()
+            addTag(tag)
+            editTag.text.clear()
+        }
+    }
 
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -26,11 +36,13 @@ class FilterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         setContentView(R.layout.activity_filter)
         toolbar.title.text = "Filters"
         restoreFilters()
+        restoreChips()
         sectionList.onItemSelectedListener = this
         sortList.onItemSelectedListener = this
         dateRangeList.onItemSelectedListener = this
         searchSortList.onItemSelectedListener = this
         searchDateRangeList.onItemSelectedListener = this
+        addTagButton.setOnClickListener(this)
         formatList.onItemSelectedListener = this
         sizeList.onItemSelectedListener = this
         hideCorrectItems()
@@ -65,6 +77,54 @@ class FilterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         searchDateRangeList.setSelection(ImgurApi.getParameterSearch().timeSearch)
         formatList.setSelection(ImgurApi.getParameterSearch().type)
         sizeList.setSelection(ImgurApi.getParameterSearch().size)
+    }
+
+    private fun restoreChips() {
+        ImgurApi.getParameterSearch().tags.forEach { action -> restoreTag(action) }
+    }
+
+    private fun restoreTag(tag: String){
+        val chip = Chip(this)
+        chip.text = tag
+        //chip.setChipBackgroundColorResource(R.color.chipBackground)
+        chip.isCloseIconEnabled = true
+        chip.setOnCloseIconClickListener {
+            ImgurApi.getParameterSearch().tags.remove(chip.text.toString())
+            tagChipGroup.removeView(chip as View)
+        }
+        tagChipGroup.addView(chip)
+        if (!ImgurApi.getParameterSearch().tags.contains(tag))
+            ImgurApi.getParameterSearch().tags.add(tag)
+    }
+
+    fun addTag(tag: String) {
+        val tagChip = createTag(tag)
+        if (tagChip != null) {
+            tagChipGroup.addView(tagChip)
+            if (!ImgurApi.getParameterSearch().tags.contains(tag))
+                ImgurApi.getParameterSearch().tags.add(tag)
+        }
+    }
+
+    fun validateTag(tag: String): Boolean {
+        if (tag.isNotBlank() && !ImgurApi.getParameterSearch().tags.contains(tag) &&
+                tag.matches(Regex("[a-zA-Z]+")))
+            return true
+        return false
+    }
+
+    fun createTag(tag: String): Chip? {
+        if (!validateTag(tag))
+            return null
+        val chip = Chip(this)
+        chip.text = tag
+        //chip.setChipBackgroundColorResource(R.color.chipBackground)
+        chip.isCloseIconEnabled = true
+        chip.setOnCloseIconClickListener {
+            ImgurApi.getParameterSearch().tags.remove(chip.text.toString())
+            tagChipGroup.removeView(chip as View)
+        }
+        return chip
     }
 
     fun backButtonAction(view: View) {
